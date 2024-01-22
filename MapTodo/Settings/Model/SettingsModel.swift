56d432e28ec969cd2d5fe59b342a9e8b8800b2d3
me990928 <me990928@gmail.com>
2024-01-23 01:36:39 +0900

    作業ディレクトリの追加

---
+++ MapTodo/Settings/Model/SettingsModel.swift
@@ -0,0 +1,11 @@
+//
+//  SwiftUIView.swift
+//  MapTodo
+//
+//  Created by 広瀬友哉 on 2024/01/23.
+//
+
+import SwiftUI
+
+struct SwiftUIView {
+}
---
+++ MapTodo/Settings/ViewModel/SettingsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  SettingsViewModel.swift
+//  MapTodo
+//
+//  Created by 広瀬友哉 on 2024/01/23.
+//
+
+import Foundation
+
+class SettingsViewModel: ObservableObject {
+    
+}
